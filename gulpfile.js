68d94c227ac8377f4d21c00e4fdd586b0b1b996a
yangxiooping <yangxiaoping@jd.com>
2017-09-08 17:51:34 +0800

    创建自己的插件

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,22 @@
 * @Author: yangxiooping
 * @Date:   2017-09-08 15:35:06
 * @Last Modified by:   yangxiooping
-* @Last Modified time: 2017-09-08 15:42:38
+* @Last Modified time: 2017-09-08 17:16:17
 */
+
+// var gulp = require('gulp');
+// var postcss = require('gulp-postcss');
+// var autoprefixer = require('autoprefixer');
+// var cssnext = require('cssnext');
+// var precss = require('precss');
 
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssnext = require('cssnext');
-var precss = require('precss');
+var yxpplugin = require('postcss-yxpplugin');
 
 gulp.task('css', function () {
 	var processors = [
-	  autoprefixer,
-	  cssnext,
-	  precss
+	  yxpplugin
 	];
   return gulp.src('./src/*.css')
     .pipe(postcss(processors))
